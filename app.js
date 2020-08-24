6a5d1c336e5ac9484b4ee49474324bc5b069280f
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-08-24 12:07:06 +0530

    date filter

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const  express = require('express');
+const express = require('express');
 const app = express()
 const afterLoad = require('after-load')
 const htmlToText = require('html-to-text');
 
 const bodyparser = require('body-parser')
+
+const chrono =require('chrono-node')
 var scrapedHtml;
 var text;
 var processedText
+var dates;
 app.get('/extract', (req, res) => {
    // console.log(req.params.text)
     //console.log(req.params.query)
@@ -17,9 +20,17 @@ app.get('/extract', (req, res) => {
   
  
    text = htmlToText.fromString(scrapedHtml);
-    console.log(text);
-    processedText = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-    processedText=processedText.replace(/[^a-zA-Z 0-9]/g, " ")
+
+    processedText = text.replace(/(?:https?|ftp):\/\/[\S]+/g, ' ');
+    processedText=processedText.replace(/[^a-zA-Z 0-9/(.)/(')/(,)]/g, " ")
+    dates= chrono.parse(processedText)
+
+    dates&&dates.map(res=>{
+      processedText = processedText.slice(0, res.index) 
+                    + "<date>" 
+                    + processedText.slice(res.index); 
+    })
+
     return res.json({text:processedText});
   
 });
